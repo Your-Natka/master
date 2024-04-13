c0af9acb900973a7c5f6dcddc96c9a54ab725e0b
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-04-13 15:56:01 +0200

    start

---
+++ app.js
@@ -0,0 +1,5 @@
+// Natalya110nb  kod TcoNvsDa0j31ir4e;
+// 31.183.188.19/32
+// Nata:Y6ZTaenbD1QXpPX5
+// mongodb+srv://Nata:Y6ZTaenbD1QXpPX5@cluster0.vefigle.mongodb.net/
+// https://downloads.mongodb.com/compass/mongodb-compass-1.42.5-darwin-arm64.dmg
